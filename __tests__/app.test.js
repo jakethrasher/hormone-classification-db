b2d6782012c7e7d5412e845519f5a0d9b0faa351
Jake Thrasher <mjakethrasher@gmail.com>
2021-04-07 10:51:12 -0700

    passing test for get by ID

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,18 +6,10 @@ const Hormone = require('../lib/models/Hormone');
 
 
 describe('app routes',()=>{
-    const hormone = 
-    beforeAll(()=>{
-        return setup(pool).then(()=>{
-            return Hormone.create({
-                hormone:'antidiuretic hormone',
-                sourceOrgan:'posterior pituitary',
-                targetOrgan:'kidney tubules/smooth muscle in arterioles',
-                physiologicalAction:'regulates blood pressure by causing water reabsorption',
-                structure:'peptide hormone'
-            })
-        })
-    })
+  
+    beforeEach(()=>{
+        return setup(pool)
+    });
 
     it('creates a hormone instance', async()=>{
         const data = await request(app)
@@ -41,32 +33,42 @@ describe('app routes',()=>{
     })
 
     it('gets all hormone data', async()=>{
-        
+        const hormones = await Promise.all([
+            Hormone.create({
+                hormone:'antidiuretic hormone',
+                    sourceOrgan:'posterior pituitary',
+                    targetOrgan:'kidney tubules/smooth muscle in arterioles',
+                    physiologicalAction:'regulates blood pressure by causing water reabsorption',
+                    structure:'peptide hormone'
+            }),
+            Hormone.create({
+                hormone:'follicle stimulating hormone(FSH)',
+                sourceOrgan:'anterior pituitary',
+                targetOrgan:'ovaries/testes',
+                physiologicalAction:'Ovarian follicle growth and estrogen secretion. Spermatogenesis.',
+                structure:'glycoprotein'
+            })
+        ])
         const data = await request(app)
             .get('/api/v1/hormones')
             
-        expect(data.body[0]).toEqual({
-            id:expect.any(String),
-            hormone:expect.any(String),
-            sourceOrgan:expect.any(String),
-            targetOrgan:expect.any(String),
-            physiologicalAction:expect.any(String),
-            structure:expect.any(String),
-    })
+        expect(data.body).toEqual(expect.arrayContaining(hormones))
     })
 
     it('gets a hormone by id', async()=>{
+
+        const fsh = await Hormone.create({
+        hormone:'follicle stimulating hormone(FSH)',
+        sourceOrgan:'anterior pituitary',
+        targetOrgan:'ovaries/testes',
+        physiologicalAction:'Ovarian follicle growth and estrogen secretion. Spermatogenesis.',
+        structure:'glycoprotein'
+        })
+
         const data = await request(app)
             .get('/api/v1/hormones/1')
 
-        expect(data.body).toEqual({
-            id:expect.any(String),
-            hormone:expect.any(String),
-            sourceOrgan:expect.any(String),
-            targetOrgan:expect.any(String),
-            physiologicalAction:expect.any(String),
-            structure:expect.any(String),
-        })
+        expect(data.body[0]).toEqual(fsh)
     })
     
 
